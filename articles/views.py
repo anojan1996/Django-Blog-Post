d878eb86008e436f584a82bc49bff6796d87fbab
anojan1996 <anojan2012@gmail.com>
2021-06-26 22:03:26 +0530

    Article Detail Template

--- articles/views.py
+++ articles/views.py
@@ -9,5 +9,6 @@ def article_list(request):
     return render(request, 'articles/article_list.html', {'articles': articles})
 
 
-def article_detail(request,slug):
-    return HttpResponse(slug)
+def article_detail(request, slug):
+    article = Article.objects.get(slug=slug)
+    return render(request, 'articles/article_detail.html', {'article': article})
