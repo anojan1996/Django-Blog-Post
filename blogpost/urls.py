cfd9d19d13ea4f698e07f025ceff62ae4ac0ea99
anojan1996 <anojan2012@gmail.com>
2021-06-29 23:08:40 +0530

    Image Upload

--- blogpost/urls.py
+++ blogpost/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls.static import static
 from django.urls import path,include
-from . import views
+from . import views, settings
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
@@ -26,5 +27,6 @@ urlpatterns = [
 ]
 
 urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # python manage.py createsuperuser
