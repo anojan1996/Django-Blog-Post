d400ed13982632e3eee7e72a4a6069040290b2bd
anojan1996 <anojan2012@gmail.com>
2021-06-29 23:32:34 +0530

    Accounts App

--- blogpost/urls.py
+++ blogpost/urls.py
@@ -22,6 +22,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('articles/', include('articles.urls')),
+    path('accounts/', include('accounts.urls')),
     path('about/', views.about),
     path('', views.homepage),
 ]
